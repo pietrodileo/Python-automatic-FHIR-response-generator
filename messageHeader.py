c14da195818705adc42103d59e7d6111b0792ca3
Pietro Di Leo <114354425+pietrodileo@users.noreply.github.com>
2023-10-15 17:18:30 +0200

    Update messageHeader.py

--- messageHeader.py
+++ messageHeader.py
@@ -6,7 +6,7 @@ import os
 class MessageHeader(GenericFHIRresource):
     def __init__(self, codiceMH, displayMH):
         # Create an UUID for the resource
-        mh_uid = str(uuid.uuid4()) 
+        mh_uid = str(uuid.uuid4())
         fullUrl = "MessageHeader/" + mh_uid
         self.fullUrl = fullUrl
         self.resource = {
@@ -29,34 +29,34 @@ class MessageHeader(GenericFHIRresource):
                 "endpoint": "localhost:52773/fhir/retilab/Omr"
             }
         }
-                
-    def ExtractMessageHeaderInfo(self, resource):
-        # Extract the source code        
+
+    def ExtractMessageHeaderInfo(self, resource, initFocus):
+        # Extract the source code
         codiceApplicativoSorgente = resource['destination']['name']
-        
+
         # Open a json containing the data of all the laboratories
         my_dir = os.path.dirname(__file__)
-        json_file_path = os.path.join(my_dir, 'CensimentoEnti.json')        
+        json_file_path = os.path.join(my_dir, 'CensimentoEnti.json')
         with open(json_file_path, 'r') as file:
             json_censimento = json.load(file)
-        
+
         # Find the information about the filler laboratory
-        fillerLab = next((lab for lab in json_censimento if lab["CodiceApplicativo"] == codiceApplicativoSorgente), 0)
+        destinationLab = next((lab for lab in json_censimento if lab["CodiceApplicativo"] == codiceApplicativoSorgente), 0)
 
         # Assign the extracted parameters to the object
         self.resource['source'] = {
-            "name": fillerLab['CodiceApplicativo'],
-            "software": fillerLab['NomeStruttura'] + " - " + fillerLab['NomeLaboratorio'],
+            "name": destinationLab['CodiceApplicativo'],
+            "software": destinationLab['NomeStruttura'] + " - " + destinationLab['NomeLaboratorio'],
             #"endpoint": resource['destination']['endpoint']
-            "endpoint": fillerLab['Endpoint'] 
+            "endpoint": destinationLab['Endpoint']
         }
         self.resource['response'] = {
             "identifier": resource['id'],
             "code": "ok"
         }
-        
-        # Initialize the focus property
-        self.resource['focus'] = []
-        
-        return fillerLab
-    
+
+        if initFocus:
+            # Initialize the focus property
+            self.resource['focus'] = []
+
+        return destinationLab
